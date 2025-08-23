e250b0f5405a968f2a90b7bf048ee009878590fd
Muhammad Uzair <uzair.jay21@gmail.com>
2025-08-23 20:30:45 +0500

    added chatbot project

--- data/index.ts
+++ data/index.ts
@@ -74,11 +74,11 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    title: "Eclipso AI Chatbot",
+    des: "Minimal, dark-themed AI chatbot with a sleek UI and seamless Gemini integration.",
+    img: "/p01.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://eclipsoai.vercel.app/",
   },
   {
     id: 2,
